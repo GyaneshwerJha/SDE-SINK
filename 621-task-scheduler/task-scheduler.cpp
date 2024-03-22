794325ac4ff0e03171850c99065cfcc2a0bd4b79
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-03-22 21:17:34 +0530

    Time: 100 ms (8.32%) | Memory: 47.5 MB (8.18%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,16 +1,42 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int>mp;
-        int count = 0;
-        for(auto e : tasks)
-        {
-            mp[e]++;
-            count = max(count, mp[e]);
+        int len = tasks.size();
+        unordered_map<char, int> mp;
+        for(int i = 0; i < len; i++){
+            mp[tasks[i]]++;
         }
+
+        priority_queue<int> pq;
+        for(auto it : mp){
+            pq.push(it.second);
+        }
+
+        int minInterval = 0;
         
-        int ans = (count-1)*(n+1);
-        for(auto e : mp) if(e.second == count) ans++;
-        return max((int)tasks.size(), ans);
+        while(!pq.empty()){
+            vector<int> temp;
+            for(int i = 1; i <= n + 1; i++){
+                if(!pq.empty()){
+                    int t = pq.top();
+                    pq.pop();
+                    t--;
+                    temp.push_back(t);
+                }    
+            }    
+            for(int i = 0; i < temp.size(); i++){
+                if(temp[i] > 0){
+                  pq.push(temp[i]);
+                }
+            }
+            if(pq.empty()){
+                minInterval += temp.size();
+            }
+            else{
+                minInterval += n + 1;
+            }
+
+        }
+        return minInterval;
     }
 };
