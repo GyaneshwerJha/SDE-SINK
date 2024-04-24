9ea29a2de0ead03febd1d99dee4c773b17c9b50b
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-04-24 19:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (29.12%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<long long int> v;
-        v.push_back(0);
-        v.push_back(1);
-        v.push_back(1);
-
-        for(int i = 0; i < n; i++){
-            long long int a = v[i] + v[i + 1] + v[i + 2];
-            v.push_back(a);
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        vector<long long int> v(n + 1);
+        v[0] = 0;
+        v[1] = 1;
+        v[2] = 1;
+        for(int i = 3; i <= n; i++){
+            v[i] = v[i - 1] + v[i - 2] + v[i - 3];
         }
-
-        // for(int i = 0; i < v.size(); i++){
-        //     cout<<v[i]<<" ";
-        // }
-
         return v[n];
     }
 };
