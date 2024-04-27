8c08d2a18b6948973bb37796ead32b74c368cb2e
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-04-27 18:22:02 +0530

    Time: 231 ms (48.64%) | Memory: 18.2 MB (57.39%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -19,11 +19,35 @@ public:
         n = grid.size();
         m = grid[0].size();
 
-        vector<vector<int>> memo(n, vector<int>(m, -1));
+        // vector<vector<int>> memo(n, vector<int>(m, -1));
+
+        // int mini = INT_MAX;
+        // for(int j = 0; j < m; j++){
+        //     mini = min(mini, minSum(n - 1, j, grid, memo));
+        // }
+
+        // return mini;
+
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        for(int col = 0; col < m; col++){
+            dp[0][col] = grid[0][col];
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < m; j++){
+                int result = INT_MAX;
+                for(int nextJ = 0; nextJ < m; nextJ++){
+                    if(nextJ != j){
+                        result = min(result, grid[i][j] + dp[i - 1][nextJ]);
+                    }
+                }
+                dp[i][j] =  result;
+            }
+        }
 
         int mini = INT_MAX;
-        for(int j = 0; j < m; j++){
-            mini = min(mini, minSum(n - 1, j, grid, memo));
+        for(int col = 0; col < m; col++){
+            mini = min(mini, dp[n - 1][col]);
         }
 
         return mini;
