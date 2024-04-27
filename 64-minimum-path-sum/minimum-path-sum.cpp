d90fdde47098765f6b1685b505ab83f3f50b0905
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-04-27 15:30:09 +0530

    Time: 7 ms (59.33%) | Memory: 12.4 MB (58.65%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,38 +1,55 @@
 class Solution {
 public:
-   int solve(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& memo) {
-    if (row == 0 && col == 0) {
-        return grid[row][col];
+
+    int minSum(int n, int m, vector<vector<int>> &grid, vector<vector<int>> &memo){
+        if(n <= 0 || m <= 0) return 1e8;
+        if(n - 1 == 0 && m - 1 == 0) return  grid[n - 1][m - 1];
+
+        if(memo[n][m] != -1) return memo[n][m];
+
+        int up = grid[n - 1][m - 1] +  minSum(n - 1, m, grid, memo);
+        int down = grid[n - 1][m - 1] + minSum(n, m - 1, grid, memo);
+
+        return memo[n][m] = min(up, down);
     }
 
-    if (memo[row][col] != -1) {
-        return memo[row][col];
-    }
-
-    int up = (row > 0) ? solve(row - 1, col, grid, memo) : INT_MAX;
-    int left = (col > 0) ? solve(row, col - 1, grid, memo) : INT_MAX;
-
-    return memo[row][col] = grid[row][col] + min(up, left);
-}
     int minPathSum(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        // int minSum = 0;
-        // vector<vector<int>> memo(row, vector<int>(col, -1));
-        // return solve(row - 1, col - 1, grid, memo);
-        vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+        int n = grid.size();
+        int m = grid[0].size();
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = grid[0][0];
-                } else {
-                    int up = (i > 0) ? dp[i - 1][j] : INT_MAX;
-                    int left = (j > 0) ? dp[i][j - 1] : INT_MAX;
-                    dp[i][j] = grid[i][j] + min(up, left);
+        // vector<vector<int>> memo(n + 1, vector<int>(m + 1, -1));
+        // return minSum(n, m, grid, memo);
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }
+                else{
+                    int up = grid[i][j];
+                    if(i > 0){
+                        up += dp[i - 1][j];
+                    }
+                    else{
+                        up = 1e8;
+                    }
+
+                    int down = grid[i][j];
+                    if(j > 0){
+                        down += dp[i][j - 1];
+                    }
+                    else{
+                        down = 1e8;
+                    }
+
+                    dp[i][j] = min(up, down);
                 }
             }
         }
-        return dp[row - 1][col - 1];
+
+        return dp[n - 1][m - 1];
+        
     }
 };
