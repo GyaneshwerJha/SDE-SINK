87b81cc495575d67ae5b52d555d0e47128576838
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-05-31 22:21:57 +0530

    Time: 145 ms (92.03%) | Memory: 116.7 MB (69.18%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+        ListNode *findMid(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode *reverse(ListNode *head) {
+        ListNode *prev = nullptr;
+        ListNode *curr = head;
+        ListNode *nextNode = nullptr;
+
+        while (curr != nullptr) {
+            nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return true;
+
+        ListNode *mid = findMid(head);
+        ListNode *rev = reverse(mid);
+
+        ListNode *c1 = head;
+        ListNode *c2 = rev;
+
+        while (c2 != nullptr) { // Only need to traverse till end of the second half
+            if (c1->val != c2->val) {
+                return false;
+            }
+            c1 = c1->next;
+            c2 = c2->next;
+        }
+        return true;
+    }
+};
