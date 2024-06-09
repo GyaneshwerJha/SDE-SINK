74ffc9e5059f33aa7a437547311efb2ad0623667
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-06-09 21:32:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (83.33%) - LeetSync

--- 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.cpp
+++ 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int numberOfChild(int n, int k) {
-        n--;
-        int numRound = k / n;
-        int rem = k % n;
+        int direction = 0;
+        int j = 0;
 
-        // after round if it is left side means even
-        // if round is even means it is on the left side
-        if(numRound % 2 == 0){
-            return rem;
+        for(int i = 0; i < k; i++){
+
+            if(direction == 0){
+                j++;
+            }
+            else{
+                j--;
+            }
+            if(j == n - 1){
+                direction = 1;
+            }
+            if(j == 0){
+                direction = 0;
+            }
+
+            
         }
-        else{
-            return n - rem;
-        }
-        
+
+        return j;
     }
 };
