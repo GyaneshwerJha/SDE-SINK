88877a3c2462eecab072b7266f404b18c949f44d
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-06-14 06:33:03 +0530

    Time: 9 ms (36.89%) | Memory: 21 MB (17.84%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
         int n = seats.size();
-        priority_queue<int> pq1(seats.begin(), seats.end());
-        priority_queue<int> pq2(students.begin(), students.end());
-        
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
         int minMoves = 0;
 
         for(int i = 0; i < n; i++){
-            minMoves += abs(pq1.top() - pq2.top());
-            pq1.pop();
-            pq2.pop();
+            minMoves += abs(seats[i] - students[i]);
         }
 
         return minMoves;
