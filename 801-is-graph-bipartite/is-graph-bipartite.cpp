2c4e39131e234ee7ae70fbcd208587e52539864e
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-06-25 21:03:45 +0530

    Time: 24 ms (13.11%) | Memory: 18.5 MB (13.68%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
+    bool solve(vector<vector<int>> &graph, vector<int> color, int start){
+        int V = graph.size();
+        queue<int> q;
+        q.push(start);
+        color[start] = 0;
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+
+            for(auto it : graph[node]){
+                if(color[it] == -1){
+                    color[it] = !color[node];
+                    q.push(it);
+                }
+                else if(color[it] == color[node]){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
-        vector<int> color(V, -1); // Initialize all nodes as uncolored (-1)
+        vector<int> color(V, -1);
 
-        for (int i = 0; i < V; ++i) {
-            if (color[i] == -1) { // If the node is not colored
-                queue<int> q;
-                q.push(i);
-                color[i] = 1; // Start coloring with 1
-
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
-
-                    for (int neighbor : graph[node]) {
-                        if (color[neighbor] == -1) { // If the neighbor is not colored
-                            color[neighbor] = 1 - color[node]; // Color with the opposite color
-                            q.push(neighbor);
-                        } else if (color[neighbor] == color[node]) { // If the neighbor has the same color
-                            return false;
-                        }
-                    }
+        for(int i = 0; i < V; i++){
+            if(color[i] == -1){
+                if(solve(graph, color, i) == false){
+                    return false;
                 }
             }
         }
