bee67551e32d22c6b3ed9e228fcd35e12f712d4d
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-07-09 19:46:21 +0530

    Time: 2 ms (58.69%) | Memory: 7 MB (60.92%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
 
     bool divisorGame(int n) {
-        // base case
-        if(n == 1){
-            return false;
-        }
-        for(int x = 1; x < n; x++){
-            if(n % x == 0){
-                return !divisorGame(n - x);
-            }
-        }
+        // // base case
+        // if(n == 1){
+        //     return false;
+        // }
+        // for(int x = 1; x < n; x++){
+        //     if(n % x == 0){
+        //         return !divisorGame(n - x);
+        //     }
+        // }
 
+        // return false;
+
+        if(n % 2 == 0) return true;
         return false;
     }
 };
