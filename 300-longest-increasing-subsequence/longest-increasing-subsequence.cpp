385df4d8ee0fcfa74fe27196981da0f0a4a67ccc
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-07-10 19:36:24 +0530

    Time: 799 ms (5.07%) | Memory: 295.3 MB (5.96%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    int LIS(int ind, int n, int prev_ind, vector<int> &nums, vector<vector<int>> &memo){
-        if(ind == n) return 0;
-
-        if(memo[ind][prev_ind + 1] != -1) return memo[ind][prev_ind + 1];
-
-        // take
-        int take = 0; 
-        if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
-            take = 1 + LIS(ind + 1, n, ind, nums, memo);
+    int solve(int ind, int n, vector<int> &nums, vector<int> &temp, int prev, vector<vector<int>> &memo){
+        if(ind == n){
+            return memo[ind][prev + 1] = 0;
         }
 
-        // not take
-        int notTake = LIS(ind + 1, n, prev_ind, nums, memo);
+        if(memo[ind][prev + 1] != -1) return memo[ind][prev + 1];
 
-        return memo[ind][prev_ind + 1] = max(take, notTake);
+        int take = 0;
+        if(prev == -1 || nums[ind] > nums[prev]){
+            temp.push_back(nums[ind]);
+            take = 1 + solve(ind + 1, n, nums, temp, ind, memo);
+            temp.pop_back();
+        }
+
+        int notTake = 0 + solve(ind + 1, n, nums, temp, prev, memo);
+        
+        return memo[ind][prev + 1] = max(take, notTake);
     }
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-
-        // vector<vector<int>> memo(n, vector<int>(n + 1, -1));
-        // return LIS(0, n, -1, nums, memo);
-
-        vector<int> dp(n, 1);
-        int maxEle = INT_MIN;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[i], dp[j] + 1);
-                    maxEle = max(maxEle, dp[i]);
-                }
-            }
-        }
-
-        return maxEle != INT_MIN ? maxEle : 1;
-      
+        int prev = -1;
+        int ind = 0;
+        vector<int> temp;
+        vector<vector<int>> memo(n + 1, vector<int>(n + 1, -1));
+        return solve(ind, n, nums, temp, prev, memo);
     }
 };
