831cc0db19aa00e1f5462152f7e430ba4733be1a
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-07-25 12:00:26 +0530

    Time: 533 ms (5.37%) | Memory: 214.6 MB (6.91%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        int n = nums.size();
+        multiset<int> st;
+        for(int i = 0; i < n; i++){
+            st.insert(nums[i]);
+            if(st.size() == k){
+                int maxEle = *st.rbegin();
+                ans.push_back(maxEle);
+                int delInd = i - k + 1;
+                if (delInd >= 0) {
+                    st.erase(st.find(nums[delInd]));
+                }           
+            }
+        }
+
+        return ans;
+    }
+};
