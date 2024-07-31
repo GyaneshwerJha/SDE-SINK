6ee94937e1d9df80333800a5b59e4fb15b39115f
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-07-31 14:09:01 +0530

    Time: 9 ms (17.02%) | Memory: 20.3 MB (29.70%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-    int memo[1001][1001];
+    // int memo[1001][1001];
+    unordered_map<string, int> mp;
+
     int solve(int ind, int currHeight, int n, vector<vector<int>> &books, int shelfWidth, int remainingWidth) {
         if (ind == n) return currHeight;
 
-        if(memo[ind][remainingWidth] != -1) return memo[ind][remainingWidth];
+        string key = to_string(ind) + "_" + to_string(currHeight) + "_" + to_string(remainingWidth);
+        if(mp.find(key) != mp.end()) return mp[key];
         // Take
         int take = INT_MAX;
         if (books[ind][0] <= remainingWidth) {
@@ -14,12 +17,11 @@ public:
         // not Take
         int notTake = currHeight + solve(ind + 1, books[ind][1], n, books, shelfWidth, shelfWidth - books[ind][0]);
 
-        return memo[ind][remainingWidth] = min(take, notTake);
+        return mp[key] = min(take, notTake);
     }
 
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
         int n = books.size();
-        memset(memo, -1, sizeof(memo));
         return solve(0, 0, n, books, shelfWidth, shelfWidth);
     }
 };
