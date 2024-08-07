1ca76935022869a64460f0090fe43c29e3635ae9
Gyaneshwer Jha <keshavk72411@gmail.com>
2024-08-07 18:07:23 +0530

    Time: 134 ms (18.15%) | Memory: 27.6 MB (16.91%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,19 +1,57 @@
 class Solution {
 public:
+    static bool sortByFreq(const pair<char, int> &a, const pair<char, int> &b){
+        return a.second > b.second;
+    }
+
     int minimumPushes(string word) {
-        vector<int> letterFreq(26, 0);
-        for (char c : word) {
-            letterFreq[c - 'a']++;
+        int len = word.size();
+        unordered_map<char, int> mp;
+
+        for(int i = 0; i < len; i++){
+            mp[word[i]]++;
+        }
+
+        vector<pair<char, int>> v;
+        for(auto it : mp){
+            v.push_back(it);
+        }
+
+        sort(v.begin(), v.end(), sortByFreq);
+        
+        for(auto it : v){
+            cout<<it.first<<" -> "<<it.second<<endl;
+        }
+
+        int cnt = 1;
+        int mini = 0;
+        for(auto it : v){
+            int ele = it.first;
+            int freq = it.second;
+            if(cnt <= 8){
+                mini += freq;
+                cout<<"count"<<cnt<<" ";
+                cout<<mini<<endl;
+            }
+            else if(cnt > 8 && cnt <= 16){
+                mini += 2 * freq;
+                cout<<"count"<<cnt<<" ";
+                cout<<mini<<endl;
+            }
+            else if(cnt > 16 && cnt <= 24){
+                mini += 3 * freq;
+                cout<<"count"<<cnt<<" ";
+                cout<<mini<<endl;
+            }
+            else{
+                mini += 4 * freq;
+                cout<<"count"<<cnt<<" ";
+                cout<<mini<<endl;
+            }
+            cnt++;
+            // cout<<cnt<<endl;
         }
         
-        sort(letterFreq.rbegin(), letterFreq.rend());
-        
-        int totalPresses = 0;
-        for (int i = 0; i < 26; i++) {
-            if (letterFreq[i] == 0) break;
-            totalPresses += (i / 8 + 1) * letterFreq[i];
-        }
-        
-        return totalPresses;
+        return mini;
     }
 };
